79e3be8656914af9dff0802895db93f2090b7ba2
Irina Illustrova <illustrova@gmail.com>
2021-04-06 12:03:03 +0300

    fix add-component script

--- design-system/src/index.js
+++ design-system/src/index.js
@@ -3,12 +3,6 @@
  * It's being updated automatically by add-component script
  * Don't edit it directly, your chages will be overwritten.
  */
-export { default as Ample8 } from "./components/Ample8";
+export { default as GlobalStyle } from "src/global.js";
 export { default as Button } from "./components/Button";
-export { default as GlobalStyle } from "./components/GlobalStyle";
 export { default as Icon } from "./components/Icon";
-export { default as Sample2 } from "./components/Sample2";
-export { default as Sample3 } from "./components/Sample3";
-export { default as ample3 } from "./components/ample3";
-export { default as ample4 } from "./components/ample4";
-export { default as ample6 } from "./components/ample6";
