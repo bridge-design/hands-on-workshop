f5f4629780b8c237247f0786bd7572e774b3ff21
Irina Illustrova <illustrova@gmail.com>
2021-04-14 15:21:11 +0300

    move tokens to src/tokens

---
+++ design-system/src/tokens/dist/colors.js
@@ -0,0 +1,11 @@
+/**
+ * Do not edit directly
+ * Generated on Wed, 14 Apr 2021 12:17:39 GMT
+ */
+
+export const secondaryBlack100 = "rgba(4, 4, 21, 1)";
+export const secondaryBlack80 = "rgba(4, 4, 21, 0.8)";
+export const secondaryBlack60 = "rgba(4, 4, 21, 0.6)";
+export const secondaryBlack40 = "rgba(4, 4, 21, 0.4)";
+export const secondaryBlack20 = "rgba(4, 4, 21, 0.2)";
+export const secondaryBlack10 = "rgba(4, 4, 21, 0.1)";
