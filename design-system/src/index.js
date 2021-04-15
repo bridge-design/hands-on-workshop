63b9915fdef093110ee887c4fd46da179032c325
Irina Illustrova <illustrova@gmail.com>
2021-04-15 23:23:03 +0300

    add summarytable component

--- design-system/src/index.js
+++ design-system/src/index.js
@@ -7,7 +7,8 @@
 export { default as GlobalStyle } from "./global.js";
 export { colors, typography } from "./tokens";
 
-export { default as Avatar } from "./components/Button";
+export { default as Avatar } from "./components/Avatar";
 export { default as Button } from "./components/Button";
 export { default as Icon } from "./components/Icon";
 export { default as Select } from "./components/Select";
+export { default as SummaryTable } from "./components/SummaryTable";
