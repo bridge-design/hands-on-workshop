fd4bedf4ce670ab5517ef93ba3d2ad279b9e764b
Irina Illustrova <illustrova@gmail.com>
2021-04-15 22:46:46 +0300

    add select component

--- design-system/src/index.js
+++ design-system/src/index.js
@@ -5,7 +5,9 @@
  */
 
 export { default as GlobalStyle } from "./global.js";
-export { colors, typography } from "./tokens/dist";
 
+
+export { default as Avatar } from "./components/Avatar";
 export { default as Button } from "./components/Button";
 export { default as Icon } from "./components/Icon";
+export { default as Select } from "./components/Select";
