543c1f2244d0222b562b5a4d1dff115c1d56d309
Irina Illustrova <illustrova@gmail.com>
2021-04-16 02:17:46 +0300

    implement summarytable into product

--- product/src/pages/cart.js
+++ product/src/pages/cart.js
@@ -4,19 +4,13 @@
  */
 import React from "react";
 import styled from "styled-components";
-import { Button, Icon } from "design-system";
+import { Avatar, Button, Icon, SummaryTable } from "design-system";
 
 import avatarImg from "../assets/avatar.png";
 import item1Img from "../assets/item1.png";
 
 const StyledDiv1 = styled.div`
   padding: 64px 40px;
-`;
-
-const StyledImage = styled.img`
-  width: 48px;
-  height: 48px;
-  display: block;
 `;
 
 const Styledh1 = styled.h1`
@@ -45,13 +39,6 @@ const StyledDiv4 = styled.div`
 
 const StyledDiv5 = styled.div`
   margin-top: 32px;
-  /* padding: 30px 35px 35px 35px; */
-`;
-const StyledDiv6 = styled.div`
-  display: grid;
-  grid-template-columns: 1fr auto;
-  row-gap: 16px;
-  margin-bottom: 32px;
 `;
 
 const StyledImg = styled.img`
@@ -75,9 +62,6 @@ const StyledP = styled.p`
   opacity: 0.6;
 `;
 
-const Styledh2 = styled.h2`
-  margin-bottom: 30px;
-`;
 const StyledBorder = styled.div`
   width: 100%;
   height: 1px;
@@ -85,17 +69,12 @@ const StyledBorder = styled.div`
   margin: 32px 0;
 `;
 
-const BiggerP = styled.p`
-  font-size: 18px;
-  line-height: 28px;
-`;
-
 const Cart = () => {
   return (
     <div>
       <StyledDiv1>
         <StyledDiv2>
-          <StyledImage src={avatarImg} />
+          <Avatar src={avatarImg} />
           <Button isOutline>
             <Icon name="menu" />
           </Button>
@@ -113,18 +92,13 @@ const Cart = () => {
         <StyledBorder />
 
         <StyledDiv5>
-          <Styledh2>Summary</Styledh2>
-          <StyledDiv6>
-            <p>Subtotal</p>
-            <h6>€95.00</h6>
-            <p>Estimated Delivery & Handling</p>
-            <h6>€0.00</h6>
-          </StyledDiv6>
-          <StyledBorder />
-          <StyledDiv6>
-            <BiggerP>Total</BiggerP>
-            <h4>€95.00</h4>
-          </StyledDiv6>
+          <SummaryTable
+            items={[
+              { name: "Subtotal", price: "€95.00" },
+              { name: "Estimated Delivery & Handling", price: "€0.00" },
+            ]}
+            total="€95.00"
+          />
           <Button isStretched>Checkout</Button>
         </StyledDiv5>
       </StyledDiv1>
