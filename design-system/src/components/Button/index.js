d5cdb7df0575f6b271a761b992a0389c74e52d32
Irina Illustrova <illustrova@gmail.com>
2021-04-16 02:56:51 +0300

    added active state to button

--- design-system/src/components/Button/index.js
+++ design-system/src/components/Button/index.js
@@ -16,7 +16,11 @@ const StyledButton = styled.button`
   outline: none;
   border: 1px solid;
   border-color: ${(p) =>
-    p.isOutline ? colors.secondaryBlack20 : "transparent"};
+    p.isOutline
+      ? p.isActive
+        ? colors.pinkPink100
+        : colors.secondaryBlack20
+      : "transparent"};
   box-sizing: border-box;
   line-height: 24px;
   position: relative;
@@ -24,13 +28,26 @@ const StyledButton = styled.button`
   text-decoration: none;
   border-radius: 1rem;
   padding: ${(p) => (p.label ? "16px" : "12px")};
-  background-color: ${(p) => (p.isOutline ? "transparent" : p.color)};
+  background-color: ${(p) =>
+    p.isOutline ? (p.isActive ? colors.pinkPink20 : "transparent") : p.color};
   opacity: ${(p) => (p.disabled ? 0.6 : 1)};
   color: ${(p) =>
-    p.isOutline ? colors.secondaryBlack100 : colors.secondaryWhite100};
+    p.isOutline
+      ? p.isActive
+        ? colors.pinkPink80
+        : colors.secondaryBlack100
+      : colors.secondaryWhite100};
   font-size: 14px;
   font-weight: 700;
   width: ${(p) => (p.isStretched ? "100%" : null)};
+  ${(p) =>
+    p.isActive &&
+    `svg {
+      fill: ${colors.pinkPink100};
+        path {
+          fill-opacity: 1;
+        }
+  }`}
 `;
 
 const Button = ({ href, label, children, to, ...props }) => {
