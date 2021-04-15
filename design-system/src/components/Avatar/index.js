b77e541860bf33815801b9f2156384dec6ffa6fe
Irina Illustrova <illustrova@gmail.com>
2021-04-15 22:57:55 +0300

    add avatar component

---
+++ design-system/src/components/Avatar/index.js
@@ -0,0 +1,20 @@
+import { string } from "prop-types";
+import React from "react";
+import styled from "styled-components";
+
+/* Avatar
+ *
+ * Avatar displays user picture
+ */
+const Avatar = ({src, ...props}) => <StyledAvatar src={src} />;
+
+const StyledAvatar = styled.img`
+  width: 48px;
+  height: 48px;
+`;
+
+Avatar.propTypes = {
+  src: string,
+}
+
+export default Avatar;
