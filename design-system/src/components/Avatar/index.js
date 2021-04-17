31240112e93fd912ba91dbc02c1e48176539d3d2
Irina Illustrova <illustrova@gmail.com>
2021-04-17 18:17:23 +0300

    change assets in storybook

--- design-system/src/components/Avatar/index.js
+++ design-system/src/components/Avatar/index.js
@@ -6,15 +6,16 @@ import styled from "styled-components";
  *
  * Avatar displays user picture
  */
-const Avatar = ({src, ...props}) => <StyledAvatar src={src} />;
+const Avatar = ({ src, ...props }) => <StyledAvatar src={src} />;
 
 const StyledAvatar = styled.img`
   width: 48px;
   height: 48px;
+  border-radius: 16px;
 `;
 
 Avatar.propTypes = {
   src: string,
-}
+};
 
 export default Avatar;
